ef00340d3ac8e5e072ee6f00f49a13aacfb04d6d
minkolee <minkolee@gmail.com>
2020-08-24 17:26:42 +0800

    继续使用React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch, Link} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props)=>{
+    return(
+    <div>
+        <h1>HATS PAGE</h1>
+        <p>This is No.{props.match.params.hatsId} hat.</p>
+    </div>
+)}
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch>
+        <Route path='/hats/:hatsId' component = {HatsPage}/>
+        <Route path='/' component = {HomePage}/>
+    </Switch>
+
+    <Link to='/hats/11'>No.11</Link>
     </div>
   );
 }
+
 
 export default App;
