bcb3595bbd87b765709e6a1e12bad8c6d2ca5e91
minkolee <minkolee@gmail.com>
2020-08-26 15:31:38 +0800

    调好了商店页面

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './App.css';
 import {Route, Switch, Link} from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component';
 
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from "./components/header/header.component";
 
 function App() {
-  return (
-    <div>
-        <Route exact path='/' component = {HomePage}/>
-    </div>
-  );
+    return (
+        <div>
+            <Header></Header>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route exact path='/shop' component={ShopPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 
